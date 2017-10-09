c8032773e12445d908af4d32c4c0cc186bc14153
GabrielLicona <gabriel@peeyrapp.com>
2017-10-09 03:16:15 -0400

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,207 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { InfoHtml } from './infoHtml';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit{
+
+	greeting: string = 'Hi! ';
+	nouns: string[] = ["ENGINEER", "DEVELOPER", "ADVENTURER","TINKERER", "LEARNER", "TRAVELER", 
+  "ENTREPRENEUR", "EXPERIMENTER", "ANALYZER", "READER", "OBSERVER", "FOODIE", "TECHNOLOGIST", 
+  "CREATOR", "STRATEGIST", "INNOVATOR", "IDEATOR"];
+  	info: string[] = InfoHtml;
+  	sections: any[] = [{
+  		title: 'Experience',
+  		active: false,
+  		content: this.info[0],
+  		left: true
+  	}, {
+   		title: 'Education', 		
+  		active: false,
+  		content: this.info[1],
+  		left: true
+  	}, {
+   		title: 'Projects',
+  		active: false,
+  		content: this.info[2],
+  		left: true
+  	}, {
+  		title: 'Skills',
+  		active: false,
+  		content: this.info[3],
+  		left: true
+  	}, {
+   		title: 'Contact',
+  		active: false,
+  		content: this.info[4],
+  		left: true
+  	}];
+  	clickable: boolean = false;
+  	isActive: boolean = false;
+
+	ngOnInit () {
+		this.typeText('My name is Gabe Licona', 100, 1200).then( (res) => {
+			this.swipeSections(false, -1).then( (res) => {
+				this.clickable = true;
+				this.backspace(0, 15, 75, 2500).then( (res) => {
+					this.typeText(': ', 100, 500).then( (res) => {
+						this.switchNouns();
+					});
+				});
+			});
+		});
+	}
+
+	async switchNouns () {
+		this.nouns = this.shuffleArray(this.nouns);
+		for(var i = 0; i < this.nouns.length; i++) {
+			await this.typeText(this.nouns[i], 100, 1200);
+			await this.backspace1(this.nouns[i].length - 1, 75, 1000);
+			if(i === this.nouns.length - 1) {
+				i = -1;
+				this.nouns = this.shuffleArray(this.nouns);
+			}
+		}
+	}
+
+
+	typeText = (text, speed, delay) => {
+		var initialText = this.greeting;
+		var counter = 0;
+		return new Promise( (resolve, reject) => {
+			try {
+				setTimeout(() => {
+					var interval = setInterval( () => {
+						if (counter > text.length) {
+							clearInterval(interval);
+							resolve('done');
+						} else {
+							this.greeting = initialText.concat(text.substr(0, counter));
+							counter++;
+						}
+					}, speed);
+				}, delay);
+			} catch (e) {
+				reject(e);
+			}
+		});
+	}
+
+	backspace = (leftIndex, rightIndex, speed, delay) => {
+		var removeStr = this.greeting.slice(leftIndex, rightIndex);
+		var leftStr = this.greeting.slice(0, leftIndex);
+		var rightStr = this.greeting.slice(rightIndex);
+		var counter = 0;
+		return new Promise ( (resolve, reject) => {
+			try {
+				setTimeout(() => {
+					var interval = setInterval( () => {
+						if(counter > removeStr.length) {
+							clearInterval(interval);
+							resolve('done');
+						} else {
+							this.greeting = leftStr.concat(removeStr.slice(leftIndex, rightIndex - counter)).concat(rightStr);
+							counter++;
+						}
+					}, speed);
+				}, delay);
+			} catch (e) {
+				reject(e);
+			}
+		});
+	}
+
+	backspace1 = (length, speed, delay) => {
+		var counter = 0;
+		var origString = this.greeting;
+		return new Promise ( (resolve, reject) => {
+			try {
+				setTimeout(() => {
+					var interval = setInterval( () => {
+						if(counter > length) {
+							clearInterval(interval);
+							resolve('done');
+						} else {
+							this.greeting = this.greeting.slice(0, this.greeting.length - 1);
+							counter++;
+						}
+					}, speed);
+				}, delay);
+			} catch (e) {
+				reject(e);
+			}
+		});
+	}
+
+	shuffleArray (array: any[]): any[] {
+	  let currentIndex = array.length, temporaryValue, randomIndex;
+	  // While there remain elements to shuffle...
+	  while (0 !== currentIndex) {
+	    // Pick a remaining element...
+	    randomIndex = Math.floor(Math.random() * currentIndex);
+	    currentIndex -= 1;
+	    // And swap it with the current element.
+	    temporaryValue = array[currentIndex];
+	    array[currentIndex] = array[randomIndex];
+	    array[randomIndex] = temporaryValue;
+	  }
+  		return array;
+	};
+
+	titleClicked (index: number) {
+		if(!this.clickable) {
+			return;
+		}
+		this.clickable = false;
+		for(var i = 0; i < this.sections.length; i++) {
+			if(this.sections[i].active  === true) {
+				if(i !== index) {
+					return;
+				}
+			}
+		}
+
+		if(this.sections[index].active === false) {
+			this.swipeSections(true, index).then( (res) => {
+				this.sections[index].active = true;
+				this.clickable = true;
+				this.isActive = true;
+			} );
+		} else {
+			this.sections[index].active = false;
+			this.swipeSections(false, index).then( (res) => {
+				this.clickable = true;
+				this.isActive = false;
+			});
+		}
+	}
+
+	swipeSections (left: boolean, index: number) {
+		return new Promise ((resolve, reject) => {
+			try {
+				var counter = 0;
+				var interval = setInterval(() => {
+					if (counter >= this.sections.length) {
+						clearInterval(interval);
+						resolve('done');
+					} else {
+						if (counter !== index) {
+							this.sections[counter].left = left;
+						}
+						counter++;
+					}
+				}, 100);
+			} catch (e) {
+				reject(e);
+			}
+		});
+	}
+
+	transitionEnd (e) {
+		console.log(e);
+	}
+  
 }
---
+++ src/app/infoHtml.ts
@@ -0,0 +1,152 @@
+export const InfoHtml = [
+	`
+	<div>
+		<h2>CITIGROUP | ASSISTANT VICE PRESIDENT</h2>
+		<h3>July 2016 - Present | NEW YORK, NY</h3>
+		<p>Currently working at Citi's Venture Capital group building internal
+		startups.  I function as a technical cofounder for early stage projects 
+		and provide technical expertise and business strategy to ensure success.
+		I also contribute as a front end software engineer mostly working on Angular 
+		based applications. 
+		</p>
+		<br>
+		<h2>PEEYR | COFOUNDER & CTO</h2>
+		<h3>September 2015 - July 2016 | Dallas, TX</h3>
+		<p>Peeyr is a peer-to-peer tutoring marketplace aimed at providing 
+		quality tutoring services to high school students.  Peeyr 
+		makes high school tutoring much more accessible and affordable. I 
+		developed the entire Peeyr web platform and managed a team that 
+		consisted of an iOS engineer, web developer, and product designer. 
+		I led sprints using agile and lean methodologies and defined the 
+		company's engineering culture.</p>
+		<br>
+		<h2>INFUSION | ASSOCIATE CONSULTANT</h2>
+		<h3>June 2015 - September 2015 | New York, NY</h3>
+		<p>Infusion is a creative technology consulting firm. My clients 
+		included Samsung, Re/Max, and the 9/11 Memorial Foundation. I worked 
+		on Samsung's Interactive Theater which is in their very first flagship 
+		store in New York.  The Interactive Theater is an interactive room that's 
+		used to sell Samsung Business Solutions to Samsung's largest clients. I 
+		co-developed the Re/Max Apple Watch app that displays the nearest real estate 
+		listings. I designed new layouts for the 9/11 Memorial Foundation Windows 10 
+		app.</p>
+		<br>
+		<h2>CROWDTORCH | TECHNOLOGY INTERN</h2>
+		<h3>June 2014 - August 2014 | Austin, TX</h3>
+		<p>I worked on Crowdtorch's Mobile Platform which creates event applications 
+		for clients.  I worked on the front end of an essential streaming radio 
+		feature used by music festival clients for a more engaging music experience 
+		within their event app.</p>
+		<br>
+		<h2>SELF EMPLOYED | ANDROID DEVELOPER</h2>
+		<h3>June 2011 - June 2012 | El Paso, TX</h3>
+		<p>I developed Android apps that provided unique user experiences by replacing 
+		the default home app. I generated revenues of about $90,000 and a download 
+		count of approximately 50,000 downloads. Apps reached #1 on the "Top New Paid" 
+		and top 5 on the "Top Paid" list in the Personalization category.</p>
+		<br>
+	</div>`,
+
+	`
+	<div>
+		<h2>THE UNIVERSITY OF TEXAS AT AUSTIN</h2>
+		<p>Bachelors of Science and Arts in Computer Science</p>
+		<p>Business Foundations Certificate</p>
+		<p>Presidential Achievement Scholar</p>
+		<p>June 2016</p>
+		<p>GPA: 3.3</p>
+		<br>
+		<h2>UNIVERSIDAD PONTIFICIA COMILLAS</h2>
+		<p>Study Abroad in Madrid, Spain</p>
+		<p>Spring, 2015</p>
+		<br>
+		<h2>LOTS OF BOOKS</h2>
+		<br>
+		<h2>WORLD WIDE WEB</h2>
+		<br>
+	</div>`,
+
+	`
+	<div>
+		<h2>GARZON</h2>
+		<p>Building a chatbot that helps restaurants retain customers, book reservations
+		more efficiently, reduce wait staff, and automate ordering processes.  First iteration
+		will allow users to get information on a restaurant and book reservations straight from
+		their messaging app.  The following iterations will focus on streamlining the ordering 
+		process at casual sit down restaurants.</p>
+		<br>
+		<h2>CHATBOT PHONE UI</h2>
+		<p>An experiment to test the feasability of a messaged based UI phone.  It's an Android app
+		that replaces the home screen with a chatbot.  Everything that you can do on your phone can be
+		done or accessed through the chatbot.  I believe that voice and messaging based interfaces is the
+		future of how we interact with tech.  This experiment provides a fun new way to interact with your
+		phone.</p>
+		<br>
+		<h2>SATUS</h2>
+		<p>Satus is a summer program for high school and early college students 
+		meant to inspire and encourage entrepreneurship as a viable career option. 
+		We teach our fellows how to practically start a start up and motivate them 
+		to take action.  The next SATUS program will take place in summer 2017 in 
+		Dallas, TX and is sponsored by Blackstone.</p>
+		<br>
+		<h2>PERSONAL WEALTH MANAGEMENT ALEXA SKILL</h2>
+		<p>During a fintech hackathon my team built an Alexa Skill to help first time personal investors.  
+		You can ask Alexa how much money you have in your savings account, the stock price of a specific company,
+		and how much money you could have potentially made had you invested some of your savings in the stock 
+		market.  The Alexa Skill was targeted towards first time investors so that they can get an initial feel of
+		how vmuch money they can make or lose investing in specific stocks.</p>
+		<br>
+		<h2>PERSONAL WEBSITE</h2>
+		<p>The website you're on now! Because I am not a designer by trade I wanted, I wanted to test my 
+		design skills.  The goal was to create as minimalist a design as possible will still portraying a 
+		fair representation of myself.  And this was the result! I hope you enjoy it.</p>
+		<br>
+		<h2>WII REMOTE CONTROLLED CAR</h2>
+		<p>I am a huge fan of the Arduino microcontroller and found a great library 
+		that connected to the accelerometer of the Wii nunchuck remote. I came up with the 
+		idea of using the remote's accelerometer to control an r/c car.  The Arduino 
+		provides power to the remote and the remote sends the coordinates to the Arduino. 
+		The Arduino then completes the circuit of a specific direction on the r/c car's remote 
+		based on the received coordinates.</p>
+		<br>
+	</div>`,
+
+	`
+	<div>
+		<h3>Some technical things I'm pretty good at:</h3>
+			<ul>
+				<li>Angular</li>
+				<li>Business Strategy</li>
+				<li>HTML/CSS</li>
+				<li>Javascript</li>
+				<li>Marketing</li>
+				<li>Product Management</li>
+				<li>Product Development</li>
+				<li>Tech Consulting</li>
+				<li>Typescript</li>
+				<li>User Experience</li>
+			</ul>
+		<h3>Some techincal things I need to brush up on:</h3>
+			<ul>
+				<li>Android</li>
+				<li>Arduino</li>
+				<li>AWS</li>
+				<li>Ionic</li>
+				<li>Java</li>
+				<li>MongoDB</>
+				<li>Node.js</li>
+				<li>Swift/iOS</li>
+			</ul>
+	</div>`,
+
+	`
+	<div>
+		<p><strong>Email:</strong> gabelic123@gmail.com</p>
+		<p><strong>Phone:</strong> 915-479-0653</p>
+		<br>
+		<p>I currently live in New York City and love meeting new people
+		who are interested in technology and entrepreneurship. So if you're
+		in the area contact me and we can schedule a time to meet up.</p>
+		<br>
+	</div>`
+]
